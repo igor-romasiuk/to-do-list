4821f51a85316592f44e4cdd02c4ea33e8b2bf8f
Igor <igor132143@gmail.com>
2023-07-15 14:32:36 +0300

    added styles and JS logic

---
+++ app.js
@@ -0,0 +1,17 @@
+const todoInput = document.querySelector('.todo-input');
+const todoButton = document.querySelector('.todo-button');
+const todoList = document.querySelector('.todo-list');
+
+todoButton.addEventListener('click', addTodo);
+
+function addTodo(event) {
+    event.preventDefault();
+
+    const todoDiv = document.createElement('div');
+    todoDiv.classList.add('todo');
+
+    const newTodo = document.createElement('li');
+    newTodo.innerText = 'hey';
+    newTodo.classList.add('todo-item');
+    todoDiv.appendChild(newTodo);
+}
