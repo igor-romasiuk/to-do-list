b917af9e55dfad6384fd57334f864a37dffbb1e3
Igor <igor132143@gmail.com>
2023-08-15 21:33:37 +0300

    added some JS logic and `current date`

--- app.js
+++ app.js
@@ -2,6 +2,9 @@
 const addButton = document.querySelector('.add-task-btn');
 const taskInput = document.querySelector('.input-task');
 const taskList = document.querySelector('.task-list');
+const todayDateElement = document.querySelector('.today-date');
+const taskDescription = document.querySelector('.new-task');
+const taskCompleted = document.querySelector('.checkbox');
 
 // Event listeners
 addButton.addEventListener('click', addTask);
@@ -9,7 +12,9 @@ taskInput.addEventListener('keypress', (event) => {
     if (event.key === 'Enter') {
         addTask();
     }
-})
+});
+todayDateElement.addEventListener('DOMContentLoaded', currentDate());
+taskCompleted.addEventListener('click', completedTask());
 
 // Functions
 function addTask() {
@@ -19,8 +24,13 @@ function addTask() {
         listItem.classList.add('new-task')
         listItem.innerHTML = `
             <span class="task-text">${taskText}</span>
-            <input type="checkbox" class="new-input-task">
-            <button class="delete-btn"><img src="./icons/delete-btn.png" alt="Delete"></button>
+            <div class="container-btn">
+                <label class="custom-checkbox">
+                    <input type="checkbox">
+                    <span class="checkmark"></span>
+                </label>
+                <button class="delete-btn"><img src="./icons/delete-btn.png" alt="Delete"></button>
+            </div>
         `;
         taskList.appendChild(listItem);
         taskInput.value = '';
@@ -33,4 +43,27 @@ function bindDeleteButton(listItem) {
     deleteButton.addEventListener('click', () => {
         listItem.remove()
     });
+}
+
+function currentDate() {
+    const todayDate = new Date();
+
+    const daysOfWeek = [
+        "Sunday", "Monday", "Tuesday", "Wednesday",
+        "Thursday", "Friday", "Saturday"
+    ];
+    const months = [
+        "January", "February", "March", "April", "May", "June",
+        "July", "August", "September", "October", "November", "December"
+    ];
+
+    const formattedDate = daysOfWeek[todayDate.getDay()] + ', ' +
+        months[todayDate.getMonth()] + ' ' +
+        todayDate.getDate() + ' '
+
+    todayDateElement.innerHTML = formattedDate;
+}
+
+function completedTask() {
+    taskDescription.classList.toggle('completed');
 }
