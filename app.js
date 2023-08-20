f1808d44db3c589457592ad10654d644b19ed673
Igor <igor132143@gmail.com>
2023-08-20 23:07:35 +0300

    added some JS logic

--- app.js
+++ app.js
@@ -78,27 +78,3 @@ function currentDate() {
 
     todayDateElement.innerHTML = formattedDate;
 }
-
-function filterTodo() {
-    const todos = newTask.childNodes;
-    todos.forEach(function(todo) {
-        switch (e.target.value) {
-          case "all":
-            todo.style.display = "flex";
-            break;
-          case "completed":
-            if (todo.classList.contains("completed")) {
-              todo.style.display = "flex";
-            } else {
-              todo.style.display = "none";
-            }
-            break;
-          case "uncompleted":
-            if (!todo.classList.contains("completed")) {
-              todo.style.display = "flex";
-            } else {
-              todo.style.display = "none";
-            }
-        }
-    });
-}
